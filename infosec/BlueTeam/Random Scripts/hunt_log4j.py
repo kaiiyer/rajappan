9d70c824972c5bb6bbfeea7468fe87a8d71441d1
Kai Iyer <24914913+kaiiyer@users.noreply.github.com>
2022-01-15 13:01:35 +0530

    Add files via upload
    Pull out IP & Ports from URIs

---
+++ infosec/BlueTeam/Random Scripts/hunt_log4j.py
@@ -0,0 +1,22 @@
+import sys, re
+import requests
+
+f = open('dan.txt','r')
+text = f.read()
+ips = [] 
+x = []
+re_ip = re.findall(r"\b(?:\d{1,3}\.){3}\d{1,3}\b",text)
+re_port = re.findall(r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\:(\d+)",text)
+
+if re_ip is not None:
+    for match in re_ip:
+        if match not in ips:
+            x = ips.append(match)
+            print(match)
+
+
+if re_ip is not None:
+    for match in re_port:
+        if match not in ips:
+            x = ips.append(match)
+            print(match)
